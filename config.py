a120373e0011feb809ecd85d67075deb3f371494
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-10-13 18:20:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "21803165"))
 API_HASH = getenv("API_HASH", "05e5e695feb30e25bef47484cc006da7")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "7403621976"))
+OWNER_ID = int(getenv("OWNER_ID", "5962944346"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Purvi_UPdates")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "WORLD_ALPHA")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_ALPHA_BABY_lll")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "flirtingworldchatting")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "ISHQMOHABBATT")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "mr_aman_077")
 
 # Random Start Images
 IMG = [
